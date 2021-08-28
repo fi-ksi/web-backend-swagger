a4d05c086efb881fd3394c067a0f5f5eab6738b7
Adam Hlaváček <git@adamhlavacek.com>
2021-08-28 19:47:40 +0200

    feat: add swagger-ui

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,15 @@
 import express from "express";
 import bodyParser from "body-parser";
 import { RegisterRoutes } from "../build/routes";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import swaggerUI from 'swagger-ui-dist';
+import path from "path";
 
 export const app = express();
+
+app.use('/swagger-ui', express.static(swaggerUI.absolutePath()));
+app.use('/swagger', express.static(path.resolve(path.join(__dirname, 'swagger'))));
 
 // Use body parser to read sent json payloads
 app.use(
