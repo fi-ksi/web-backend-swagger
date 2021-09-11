9329be8d164b7f85d2053134b1bfd6dc1d503c24
Adam Hlaváček <git@adamhlavacek.com>
2021-09-11 15:50:00 +0200

    feat(waves): fill

---
+++ src/endpoints/waves.ts
@@ -0,0 +1,56 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {
+    Body,
+    Get,
+    Post,
+    Route,
+    Request, Security, Path, Put, Delete,
+} from 'tsoa';
+import { WaveCreationRequest, WaveResponse, Waves, WaveUpdateRequest } from '../models/waves';
+import express from 'express';
+import { ProxyController } from '../util/proxy-controller';
+import { EmptyDict } from '../models/emptyDict';
+
+@Route('waves')
+export class EndpointWaves extends ProxyController {
+    @Get()
+    public async wavesGetAll(@Request() request: express.Request): Promise<Waves> {
+        return await this.proxy(request);
+    }
+
+    @Post()
+    @Security('ksi')
+    public async wavesCreateNew(
+        @Request() request: express.Request,
+        @Body() waveCreationRequest: WaveCreationRequest
+    ): Promise<WaveResponse> {
+        return await this.proxy(request);
+    }
+
+    @Get('{waveId}')
+    public async wavesGetSingle(
+        @Request() request: express.Request,
+        @Path() waveId: number,
+    ): Promise<WaveResponse> {
+        return await this.proxy(request);
+    }
+
+    @Put('{waveId}')
+    @Security('ksi')
+    public async wavesUpdateSingle(
+        @Request() request: express.Request,
+        @Path() waveId: number,
+        @Body() waveUpdateRequest: WaveUpdateRequest
+    ): Promise<WaveResponse> {
+        return await this.proxy(request);
+    }
+
+    @Delete('{waveId}')
+    @Security('ksi')
+    public async wavesDeleteSingle(
+        @Request() request: express.Request,
+        @Path() waveId: number,
+    ): Promise<EmptyDict> {
+        return await this.proxy(request);
+    }
+}
---
+++ src/models/waves.ts
@@ -0,0 +1,33 @@
+export interface Waves {
+    waves: Wave[];
+}
+
+export interface WaveResponse {
+    wave: Wave;
+}
+
+export interface WaveBase {
+    index: number;
+    caption: string | null;
+    garant: number;
+    time_published?: string | null;
+}
+
+export interface WaveCreate extends WaveBase {
+    year: number;
+}
+
+export interface Wave extends WaveCreate {
+    id: number;
+    public: boolean;
+    sum_points: number;
+    task_cnt: number;
+}
+
+export interface WaveUpdateRequest {
+    wave: WaveBase;
+}
+
+export interface WaveCreationRequest {
+    wave: WaveCreate;
+}
