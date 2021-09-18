621a80f9faaf93a029fd20c4397a396e548ed431
Adam Hlaváček <git@adamhlavacek.com>
2021-09-18 14:35:26 +0200

    feat(modules): fill

---
+++ src/endpoints/modules.ts
@@ -0,0 +1,34 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {
+    Body,
+    Get,
+    Post,
+    Route,
+    Request, Security, Path, UploadedFiles,
+} from 'tsoa';
+import express from 'express';
+import { ProxyController } from '../util/proxy-controller';
+import { ModuleResponse } from '../models/modules';
+
+@Route('modules')
+export class EndpointModules extends ProxyController {
+    @Security('ksi')
+    @Get('{modulesId}')
+    public async modulesGetSingle(
+        @Request() request: express.Request,
+        @Path() modulesId: number,
+    ): Promise<ModuleResponse> {
+        return await this.proxy(request);
+    }
+
+    @Security('ksi')
+    @Post('{modulesId}/submit')
+    public async modulesSubmitSingle(
+        @Request() request: express.Request,
+        @Path() modulesId: number,
+        @UploadedFiles() files?: unknown,
+        @Body() data?: string | string[],
+    ): Promise<ModuleResponse> {
+        return await this.proxy(request);
+    }
+}
---
+++ src/models/modules.ts
@@ -0,0 +1,56 @@
+export type Module = ModuleProgramming | ModuleQuiz | ModuleSortable | ModuleGeneral | ModuleText;
+
+interface ModuleBase {
+    id: number,
+    type: 'general' | 'programming' | 'quiz' | 'sortable' | 'text';
+    name: string;
+    description: string;
+    autocorrect: boolean;
+    max_score: number;
+
+    state: 'correct' | 'incorrect' | 'blank';
+    score: number | null;
+}
+
+interface ModuleProgramming extends ModuleBase {
+    code: string;
+    default_code: string;
+    last_time?: string;
+    last_origin?: string;
+}
+
+interface ModuleQuiz extends ModuleBase {
+    question: {
+        type: 'radio' | 'checkbox';
+        question: string;
+        text: string;
+        options: string[];
+    }[]
+}
+
+interface ModuleSortable extends ModuleBase {
+    sortable_list: {
+        fixed: SortableItem[];
+        movable: SortableItem[];
+    }
+}
+
+interface SortableItem {
+    content: string;
+    offset: number;
+}
+
+interface ModuleGeneral extends ModuleBase {
+    submitted_files: {
+        id: number,
+        filename: string;
+    }[];
+}
+
+interface ModuleText extends ModuleBase {
+    fields: string[];
+}
+
+export interface ModuleResponse {
+    module: Module;
+}
