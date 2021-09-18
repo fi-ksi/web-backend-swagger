555e7d6ca7d4a01ed91484df08a1e08219f8e78a
Adam Hlaváček <git@adamhlavacek.com>
2021-09-18 13:28:48 +0200

    feat(subm-files): fill

---
+++ src/endpoints/subm-files.ts
@@ -0,0 +1,31 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {
+    Get,
+    Route,
+    Request, Security, Path, Delete,
+} from 'tsoa';
+import express from 'express';
+import { ProxyController } from '../util/proxy-controller';
+import { EmptyDict } from '../models/emptyDict';
+
+@Route('submFiles')
+export class EndpointSubmFiles extends ProxyController {
+
+    @Security('ksi')
+    @Get('{submFileId}')
+    public async submFilesGetSingle(
+        @Request() request: express.Request,
+        @Path() submFileId: number,
+    ): Promise<Uint8Array> {
+        return await this.proxy(request);
+    }
+
+    @Security('ksi')
+    @Delete('{submFileId}')
+    public async subFilesDeleteSingle(
+        @Request() request: express.Request,
+        @Path() submFilesId: number,
+    ): Promise<EmptyDict> {
+        return await this.proxy(request);
+    }
+}
